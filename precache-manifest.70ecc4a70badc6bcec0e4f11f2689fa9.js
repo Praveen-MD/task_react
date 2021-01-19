467b63daa994d62af2622b91221986f1eb927291
Praveen Md <pravimd3@gmail.com>
2021-01-19 12:59:13 +0530

    Updates

---
+++ precache-manifest.70ecc4a70badc6bcec0e4f11f2689fa9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e10e044b57b9631a806",
+    "url": "/task_react/static/css/main.234ba44f.chunk.css"
+  },
+  {
+    "revision": "7e10e044b57b9631a806",
+    "url": "/task_react/static/js/main.7e10e044.chunk.js"
+  },
+  {
+    "revision": "5f91e89825a81eed703e",
+    "url": "/task_react/static/css/1.81f2b1b8.chunk.css"
+  },
+  {
+    "revision": "5f91e89825a81eed703e",
+    "url": "/task_react/static/js/1.5f91e898.chunk.js"
+  },
+  {
+    "revision": "ea60d4b33abb36680713",
+    "url": "/task_react/static/js/runtime~main.ea60d4b3.js"
+  },
+  {
+    "revision": "32fabcf961de0b92272acc29f89642b9",
+    "url": "/task_react/index.html"
+  }
+];
